09f5f16773dced0c4760a478fdff398a08496e3f
Anrose14 <83343636+Anrose14@users.noreply.github.com>
2021-08-26 02:10:05 +1100

    Day 2 Assignment submitted

---
+++ script (1).js
@@ -0,0 +1,11 @@
+//food data with properties
+
+console.log("\t FOOD DATA\n")
+console.log("Name\t\tPrice\t\tQty\n")
+
+let arr=["Biriyani\t150\t\t1","Noodles\t\t200\t\t1","Alfaham\t\t200\t\t2","Icecream\t250\t\t3","Juice\t\t160\t\t5","Laddu\t\t15\t\t1","Munch\t\t250\t\t35"];
+ 
+for(let i=0;i<7;i++)
+{
+  console.log(arr[i]);
+}
